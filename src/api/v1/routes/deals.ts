75fc6a5de1ca9c20d7c018a752e903a37e3642c8
ichrystm <chrysler.alves@controlle.com>
2021-12-01 14:39:42 -0300

    refactor: remove old routes

--- src/api/v1/routes/deals.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-// import dealsController from '../controllers/dealsController';
-
-const dealsRouter = Router();
-
-// dealsRouter.get('/', dealsController.getAllWonDeals);
-
-export default dealsRouter;
